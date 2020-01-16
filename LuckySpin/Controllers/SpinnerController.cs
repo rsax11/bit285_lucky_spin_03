b6f273b70a4e6e6b56076d03d81a649ab14030db
rsax11 <rsax1@comcast.net>
2020-01-16 15:01:50 -0800

    created models folder(Spin.cs), changed SpinnerController

--- LuckySpin/Controllers/SpinnerController.cs
+++ LuckySpin/Controllers/SpinnerController.cs
@@ -9,19 +9,25 @@ namespace LuckySpin.Controllers
 
         //The Conroller to receive the Luck number from the Route
         public IActionResult Index(int luck = 7) //Default value for luck is 7
+
+
         {
+            LuckySpin.Models.Spin spin = new LuckySpin.Models.Spin();
             int a = random.Next(1, 10);
             int b = random.Next(1, 10);
             int c = random.Next(1, 10);
 
+            
             // Load up the ViewBag for use by the Spinner View "Index.cshtml"
-            ViewBag.ImgDisplay = (a == luck || b == luck || c == luck)?"block":"none";
-            ViewBag.A = a;
-            ViewBag.B = b;
-            ViewBag.C = c;
-            ViewBag.luck = luck;
+           // ViewBag.ImgDisplay = (a == luck || b == luck || c == luck)?"block":"none";
+           // ViewBag.A = a;
+           //ViewBag.B = b;
+           // ViewBag.C = c;
+           // ViewBag.luck = luck;
 
-            return View();
+            
+
+            return View(spin);
         }
     }
 }
---
+++ LuckySpin/Models/Spin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LuckySpin.Models
+{
+    public class Spin 
+    {
+        public int a;
+        public int b;
+        public int c;
+        public string ImgDisply;
+        public int luck;
+    }
+}
